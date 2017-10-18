105152e3446fd8cc13b51c1e9bff9223b51ecc10
Travis Cunningham <travis@travis-cunningham.com>
2017-10-17 19:34:39 -0700

    chapter6 complete

--- server.js
+++ server.js
@@ -1,27 +1,22 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser')
+'use strict'
 
+const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+
+const app = express();
 app.use(express.static('static'));
 app.use(bodyParser.json());
 
-const issues = [
-  {
-    id: 1, status: 'Open', owner: 'John',
-    created: new Date('2016-08-15'), effort: 5, completionDate: undefined,
-    title: 'Error in console when clicking Add',
-  },
-  {
-    id: 2, status: 'Assigned', owner: 'Eddie',
-    created: new Date('2016-08-16'), effort: 14, completionDate: new Date('2016-08-30'),
-    title: 'Missing bottom border on panel',
-  },
-  {
-    id: 3, status: 'Assigned', owner: 'Susan',
-    created: new Date('2016-08-16'), effort: 14, completionDate: new Date('2016-08-30'),
-    title: 'Missing bottom border on panel of page 2',
-  },
-];
+app.get('/api/issues', (req, res) => {
+  db.collection('issues').find().toArray().then(issues => {
+    const metadata = { total_count: issues.length };
+    res.json({ _metadata: metadata, records: issues })
+  }).catch(error => {
+    console.log(error);
+    res.status(500).json({ message: `Internal Server Error: ${error}` });
+  });
+});
 
 const validIssueStatus = {
   New: true,
@@ -33,7 +28,6 @@ const validIssueStatus = {
 };
 
 const issueFieldType = {
-  id: 'required',
   status: 'required',
   owner: 'required',
   effort: 'optional',
@@ -45,43 +39,47 @@ const issueFieldType = {
 function validateIssue(issue) {
   for (const field in issueFieldType) {
     const type = issueFieldType[field];
-    if(!type){
+    if (!type) {
       delete issue[field];
-    } else if(type === 'required' && !issue[field]){
-      return `${field} is required`;
+    } else if (type === 'required' && !issue[field]) {
+      return `${field} is required.`;
     }
   }
 
-  if(!validIssueStatus[issue.status])
+  if (!validIssueStatus[issue.status])
     return `${issue.status} is not a valid status.`;
 
   return null;
 }
 
-app.get('/api/issues', (req, res) => {
-  const metadata = { total_count: issues.length };
-  res.json({_metadata:metadata, records:issues });
-});
-
 app.post('/api/issues', (req, res) => {
   const newIssue = req.body;
-  newIssue.id = issues.length + 1;
   newIssue.created = new Date();
-
-  if(!newIssue.status)
+  if (!newIssue.status)
     newIssue.status = 'New';
 
-  const err  = validateIssue(newIssue)
-  if (err){
-    res.status(422).json({ message: `Invalid request: ${err}` })
+  const err = validateIssue(newIssue)
+  if (err) {
+    res.status(422).json({ message: `Invalid request: ${err}` });
     return;
   }
 
-  issues.push(newIssue);
-  res.json(newIssue);
-
+  db.collection('issues').insertOne(newIssue).then(result =>
+    db.collection('issues').find({ _id: result.insertedId }).limit(1).next()
+  ).then(newIssue => {
+    res.json(newIssue);
+  }).catch(error => {
+    console.log(error);
+    res.status(500).json({ message: `Internal Server Error: ${error}` });
+  });
 });
 
-app.listen(3000, function () {
-  console.log('App started on port 3000');
+let db;
+MongoClient.connect('mongodb://localhost/issuetracker').then(connection => {
+  db = connection;
+  app.listen(3000, () => {
+    console.log('App started on port 3000');
+  });
+}).catch(error => {
+  console.log('ERROR:', error);
 });
