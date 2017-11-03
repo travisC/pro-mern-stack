e3733877d1cbb0adcadcee1d8a4542c873dc02b5
Travis Cunningham <travis@travis-cunningham.com>
2017-11-03 15:51:20 -0700

    reverted to old 2+ router package since 4+ is completely different

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, Redirect, hashHistory } from 'react-router';
 
 import IssueList from './IssueList.jsx';
+import IssueEdit from './IssueEdit.jsx';
 
-var contentNode = document.getElementById('contents');
-ReactDOM.render(<IssueList />, contentNode);      // Render the component inside the content Node
+const contentNode = document.getElementById('contents');
+const NoMatch = () => <p>Page Not Found</p>;
 
-if(module.hot) {
+const RoutedApp = () => (
+  <Router history={hashHistory} >
+    <Redirect from="/" to="/issues" />
+    <Route path="/issues" component={IssueList} />
+    <Route path="/issues/:id" component={IssueEdit} />
+    <Route path="*" component={NoMatch} />
+  </Router>
+);
+
+ReactDOM.render(<RoutedApp />, contentNode);      // Render the component inside the content Node
+
+if (module.hot) {
   module.hot.accept();
 }
---
+++ src/IssueEdit.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+export default class IssueEdit extends React.Component {
+  //eslint-disable-line
+  render() {
+    return (
+      <div>
+        <p>Placeholder for edit component. {this.props.params.id}</p>
+        <Link to="/issues">Back to issue list</Link>
+      </div>
+    );
+  }
+}
+
+IssueEdit.propTypes = {
+  params: React.PropTypes.object.isRequired,
+};
