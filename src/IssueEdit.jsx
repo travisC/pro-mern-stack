d62eeb922318c8b858e743c456029a23ced901a8
Travis Cunningham <travis@travis-cunningham.com>
2017-11-07 18:52:18 -0800

    chapter9 complete

--- src/IssueEdit.jsx
+++ src/IssueEdit.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-export default class IssueEdit extends React.Component { // eslint-disable-line
+import NumInput from './NumInput.jsx';
+import DateInput from './DateInput.jsx';
 
+export default class IssueEdit extends React.Component {
   constructor() {
     super();
     this.state = {
       issue: {
-        _id: '', title: '', status: '', owner: '', effort: '', completionDate: '', created: '',
+        _id: '', title: '', status: '', owner: '', effort: null,
+        completionDate: null, created: null,
       },
+      invalidFields: {},
     };
     this.onChange = this.onChange.bind(this);
+    this.onValidityChange = this.onValidityChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
   componentDidMount() {
@@ -23,20 +29,61 @@ export default class IssueEdit extends React.Component { // eslint-disable-line
     }
   }
 
-  onChange(event) {
+  onChange(event, convertedValue) {
     const issue = Object.assign({}, this.state.issue);
-    issue[event.target.name] = event.target.value;
+    const value = (convertedValue !== undefined) ? convertedValue : event.target.value;
+    issue[event.target.name] = value;
     this.setState({ issue });
+  }
+
+  onValidityChange(event, valid) {
+    const invalidFields = Object.assign({}, this.state.invalidFields);
+    if (!valid) {
+      invalidFields[event.target.name] = true;
+    } else {
+      delete invalidFields[event.target.name];
+    }
+    this.setState({ invalidFields });
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+
+    if (Object.keys(this.state.invalidFields).length !== 0) {
+      return;
+    }
+
+    fetch(`/api/issues/${this.props.params.id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(this.state.issue),
+    }).then(response => {
+      if (response.ok) {
+        response.json().then(updatedIssue => {
+          updatedIssue.created = new Date(updatedIssue.created);
+          if (updatedIssue.completionDate) {
+            updatedIssue.completionDate = new Date(updatedIssue.completionDate);
+          }
+          this.setState({ issue: updatedIssue });
+          alert('Updated issue successfully.');
+        });
+      } else {
+        response.json().then(error => {
+          alert(`Failed to update issue: ${error.message}`);
+        });
+      }
+    }).catch(err => {
+      alert(`Error in sending data to server: ${err.message}`);
+    });
   }
 
   loadData() {
     fetch(`/api/issues/${this.props.params.id}`).then(response => {
       if (response.ok) {
         response.json().then(issue => {
-          issue.created = new Date(issue.created).toDateString();
+          issue.created = new Date(issue.created);
           issue.completionDate = issue.completionDate != null ?
-           new Date(issue.completionDate).toDateString() : '';
-          issue.effort = issue.effort != null ? issue.effort.toString() : '';
+            new Date(issue.completionDate) : null;
           this.setState({ issue });
         });
       } else {
@@ -51,12 +98,14 @@ export default class IssueEdit extends React.Component { // eslint-disable-line
 
   render() {
     const issue = this.state.issue;
+    const validationMessage = Object.keys(this.state.invalidFields).length === 0 ? null
+      : (<div className="error">Please correct invalid fields before submitting.</div>);
     return (
       <div>
-        <form>
+        <form onSubmit={this.onSubmit}>
           ID: {issue._id}
           <br />
-          Created: {issue.created}
+          Created: {issue.created ? issue.created.toDateString() : ''}
           <br />
           Status: <select name="status" value={issue.status} onChange={this.onChange}>
             <option value="New">New</option>
@@ -69,16 +118,17 @@ export default class IssueEdit extends React.Component { // eslint-disable-line
           <br />
           Owner: <input name="owner" value={issue.owner} onChange={this.onChange} />
           <br />
-          Effort: <input size={5} name="effort" value={issue.effort} onChange={this.onChange} />
+          Effort: <NumInput size={5} name="effort" value={issue.effort} onChange={this.onChange} />
           <br />
-          Completion Date: <input
-            name="completionDate" value={issue.completionDate}
-            onChange={this.onChange}
+          Completion Date: <DateInput
+            name="completionDate" value={issue.completionDate} onChange={this.onChange}
+            onValidityChange={this.onValidityChange}
           />
           <br />
           Title: <input name="title" size={50} value={issue.title} onChange={this.onChange} />
           <br />
-          <button>Submit</button>
+          {validationMessage}
+          <button type="submit">Submit</button>
           <Link to="/issues">Back to issue list</Link>
         </form>
       </div>
