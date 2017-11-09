f346fdfe3e6559617296d7da73160c97ab5e994a
Travis Cunningham <travis@travis-cunningham.com>
2017-11-09 10:28:40 -0800

    cleanup on aisle 3

--- src/Toast.jsx
+++ src/Toast.jsx
@@ -15,11 +15,10 @@ export default class Toast extends React.Component {
 
   render() {
     return (
-      <Collapse>
+      <Collapse in={this.props.showing}>
         <div style={{ position: 'fixed', top: 30, left: 0, right: 0, textAlign: 'center' }}>
           <Alert
-            style={{ display: 'inline-block', width: 500 }}
-            bsStyle={this.props.bsStyle}
+            style={{ display: 'inline-block', width: 500 }} bsStyle={this.props.bsStyle}
             onDismiss={this.props.onDismiss}
           >
             {this.props.message}
