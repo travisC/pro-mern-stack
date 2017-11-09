8640151a614126137b67eb4764c9766824f21f70
Travis Cunningham <travis@travis-cunningham.com>
2017-11-09 10:28:07 -0800

    adding toast

--- src/IssueEdit.jsx
+++ src/IssueEdit.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { FormGroup, FormControl, ControlLabel, ButtonToolbar, Button,
-   Panel, Form, Col } from 'react-bootstrap';
+   Panel, Form, Col, Alert } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import NumInput from './NumInput.jsx';
 import DateInput from './DateInput.jsx';
+import Toast from './Toast.jsx';
 
 export default class IssueEdit extends React.Component {
   constructor() {
@@ -13,8 +14,14 @@ export default class IssueEdit extends React.Component {
         _id: '', title: '', status: '', owner: '', effort: null,
         completionDate: null, created: null,
       },
-      invalidFields: {},
+      invalidFields: {}, showingValidation: false,
+      toastVisible: false, toastMessage: '', toastType: 'success',
     };
+    this.dismissValidation = this.dismissValidation.bind(this);
+    this.showValidation = this.showValidation.bind(this);
+    this.showSuccess = this.showSuccess.bind(this);
+    this.showError = this.showError.bind(this);
+    this.dismissToast = this.dismissToast.bind(this);
     this.onChange = this.onChange.bind(this);
     this.onValidityChange = this.onValidityChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
@@ -49,7 +56,7 @@ export default class IssueEdit extends React.Component {
 
   onSubmit(event) {
     event.preventDefault();
-
+    this.showValidation();
     if (Object.keys(this.state.invalidFields).length !== 0) {
       return;
     }
@@ -66,16 +73,24 @@ export default class IssueEdit extends React.Component {
             updatedIssue.completionDate = new Date(updatedIssue.completionDate);
           }
           this.setState({ issue: updatedIssue });
-          alert('Updated issue successfully.');
+          this.showSuccess('Updated issue successfully.');
         });
       } else {
         response.json().then(error => {
-          alert(`Failed to update issue: ${error.message}`);
+          this.showError(`Failed to update issue: ${error.message}`);
         });
       }
     }).catch(err => {
-      alert(`Error in sending data to server: ${err.message}`);
+      this.showError(`Error in sending data to server: ${err.message}`);
     });
+  }
+
+  showValidation() {
+    this.setState({ showingValidation: true });
+  }
+
+  dismissValidation() {
+    this.setState({ showingValidation: false });
   }
 
   loadData() {
@@ -89,18 +104,36 @@ export default class IssueEdit extends React.Component {
         });
       } else {
         response.json().then(error => {
-          alert(`Failed to fetch issue: ${error.message}`);
+          this.showError(`Failed to fetch issue: ${error.message}`);
         });
       }
     }).catch(err => {
-      alert(`Error in fetching data from server: ${err.message}`);
+      this.showError(`Error in fetching data from server: ${err.message}`);
     });
+  }
+
+  showSuccess(message) {
+    this.setState({ toastVisible: true, toastMessage: message, toastType: 'success' });
+  }
+
+  showError(message) {
+    this.setState({ toastVisible: true, toastMessage: message, toastType: 'danger' });
+  }
+
+  dismissToast() {
+    this.setState({ toastVisible: false });
   }
 
   render() {
     const issue = this.state.issue;
-    const validationMessage = Object.keys(this.state.invalidFields).length === 0 ? null
-      : (<div className="error">Please correct invalid fields before submitting.</div>);
+    let validationMessage = null;
+    if (Object.keys(this.state.invalidFields).length !== 0 && this.state.showingValidation) {
+      validationMessage = (
+        <Alert bsStyle="danger" onDismiss={this.dismissValidation}>
+          Please correct invalid fields before submitting.
+        </Alert>
+      );
+    }
     return (
       <Panel header="Edit Issue">
         <Form horizontal onSubmit={this.onSubmit}>
@@ -176,8 +209,16 @@ export default class IssueEdit extends React.Component {
               </ButtonToolbar>
             </Col>
           </FormGroup>
+          <FormGroup>
+            <Col smOffset={3} sm={9}>{validationMessage}</Col>
+          </FormGroup>
         </Form>
-        {validationMessage}
+        <Toast
+          showing={this.state.toastVisible}
+          message={this.state.toastMessage}
+          onDismiss={this.dismissToast}
+          bsStyle={this.state.toastType}
+        />
       </Panel>
     );
   }
